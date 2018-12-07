8979d06ac9c3dae7cd91e81e4f0f47ef5531ae17
Rene <Langenre@users.noreply.github.com>
2018-12-07 11:52:38 +0100

    Create day7part2.js

---
+++ day7part2.js
@@ -0,0 +1,42 @@
+const graph = {}
+
+input.split('\n').forEach(text => {
+    const [ , pointA, pointB] = /Step (\w*) must be finished before step (\w*)/.exec(text)
+
+    if (graph[pointA] === undefined) graph[pointA] = { 
+        self: pointA, 
+        time: pointA.charCodeAt(0) - 4,
+        nbr: {}, 
+    }
+    if (graph[pointB] === undefined) graph[pointB] = { 
+        self: pointB, 
+        time: pointB.charCodeAt(0) - 4,
+        nbr: {}, 
+    }
+    graph[pointB]['nbr'][pointA] = true
+})
+
+function findPath(result, seconds, path, workers) {
+    let queue = Object
+        .values(graph)
+        .filter(points=> Object.keys(points.nbr).length === 0)
+        .sort((pointA, pointB) => pointA.self < pointB.self ? -1 : 1)
+
+    if (queue.length === 0) return seconds
+    seconds++
+    queue = queue.filter(point => workers.indexOf(point.self) === -1)
+    
+    let counter = 0
+    workers.reduce((___, worker, index) => {
+        worker = ({ ...queue[counter++] }).self || ''
+        
+        if (graph[worker] && --graph[worker].time < 1) {
+            workers[index] = ''
+            path.concat(worker)
+            Object.values(graph).forEach(point => delete point['nbr'][worker])
+            delete graph[worker]
+        }
+    }, undefined)
+
+    return findPath(result, seconds, path, workers)
+}
