1c209d9665134335453857625a1eba693494c1bb
Rene <Langenre@users.noreply.github.com>
2018-12-08 22:57:45 +0100

    Create day2.js

---
+++ day2.js
@@ -0,0 +1,18 @@
+let input = ``
+
+const result = input.split('\n').reduce((matches, text) => {
+    const chars = text.split('')
+    let counts = {}
+
+    for (let char of chars) {
+		counts[char] = (counts[char] || 0) + 1
+    }
+    
+    if (Object.values(counts).includes(2)) matches['doubles']++
+	  if (Object.values(counts).includes(3)) matches['triples']++
+    
+    return matches
+    
+}, { doubles: 0, triples: 0 })
+
+const checksum = result['doubles'] * result['triples']
