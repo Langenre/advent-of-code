e68da76c8c814ac033234446848733c03d8a3ac1
Rene <Langenre@users.noreply.github.com>
2018-12-10 10:21:42 +0100

    Update day10.js

--- day10.js
+++ day10.js
@@ -1,15 +1,1 @@
-let input = ``
-
-input = input.split('\n').map(text => {
-    let [ , x, y, vx, vy] = /position=<(.\d*), (.\d*)> velocity=<(.\d*), (.\d*)>/.exec(text)
-    return { x: parseInt(x), y: parseInt(y), vx: parseInt(vx), vy: parseInt(vy) }
-})
-
-function moveStars(input) {
-
-    input.map(star => ({ ...star, x: star.x * star.vx, y: star.y * star.vy}))
-    console.log()
-    setTimeout(() => moveStars(input), 1000)
-}
-
-moveStars(input)
+https://bugs.chromium.org/p/v8/issues/detail?id=4698
