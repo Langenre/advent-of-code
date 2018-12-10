3bb3dfe40d2f29360dcccf2998e4710f8cecc298
Rene <Langenre@users.noreply.github.com>
2018-12-10 07:24:55 +0100

    Update day10.js

--- day10.js
+++ day10.js
@@ -2,11 +2,12 @@ let input = ``
 
 input = input.split('\n').map(text => {
     let [ , x, y, vx, vy] = /position=<(.\d*), (.\d*)> velocity=<(.\d*), (.\d*)>/.exec(text)
-    return {x, y, vx, vy}
+    return { x: parseInt(x), y: parseInt(y), vx: parseInt(vx), vy: parseInt(vy) }
 })
 
 function moveStars(input) {
 
+    input.map(star => ({ ...star, x: star.x * star.vx, y: star.y * star.vy}))
     console.log()
     setTimeout(() => moveStars(input), 1000)
 }
